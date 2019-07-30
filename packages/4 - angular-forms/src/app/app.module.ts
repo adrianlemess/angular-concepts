bf59d98e46abf2d7c83ab5095cdb8c7e6b0d90d2
Adrian Lemes <ilegra.adrian@agiplan.com.br>
2019-07-30 20:59:20 -0300

    add reactive form in imports

--- packages/4 - angular-forms/src/app/app.module.ts
+++ packages/4 - angular-forms/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
@@ -10,6 +10,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    ReactiveFormsModule,
+    FormsModule,
     AppRoutingModule
   ],
   providers: [],
