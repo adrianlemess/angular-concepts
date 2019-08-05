b03048785c5a9d12ca4d575b29107200d957f221
Adrian Lemes <ilegra.adrian@agiplan.com.br>
2019-08-04 22:53:23 -0300

    updated architecture project

--- packages/1 - angular-architecture/src/app/layout/layout.module.ts
+++ packages/1 - angular-architecture/src/app/layout/layout.module.ts
@@ -1,13 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { HeaderComponent } from './header/header.component';
-import { MenuAsideComponent } from './menu-aside/menu-aside.component';
-import { FooterComponent } from './footer/footer.component';
-
-
 
 @NgModule({
-  declarations: [HeaderComponent, MenuAsideComponent, FooterComponent],
+  declarations: [],
   imports: [
     CommonModule
   ]
