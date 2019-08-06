09f0471657e2c594daa38bb5f5a79bb20b921e7e
Adrian Lemes <ilegra.adrian@agiplan.com.br>
2019-08-05 23:50:21 -0300

    add enum to the user

--- packages/3 - angular-services/src/app/shared/user.model.ts
+++ packages/3 - angular-services/src/app/shared/user.model.ts
@@ -2,7 +2,12 @@ export interface User {
   id: number;
   username: string;
   age: number;
-  role: string;
+  role: UserEnum;
   company: string;
   nationality: string;
+}
+
+export enum UserEnum {
+    ADMIN = 'admin',
+    USER = 'user'
 }
